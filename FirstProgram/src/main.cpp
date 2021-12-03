f7a0b8ded78b748dc814490b2052aa8a3155fb7b
Ahmed Harbi <ahmed.harbi.eg@gmail.com>
2021-12-03 12:45:04 +0200

    First Program

--- FirstProgram/src/main.cpp
+++ FirstProgram/src/main.cpp
@@ -1,12 +1,22 @@
+/**************************************************
+ * File: main.cpp
+ * Description: First C++ program takes number from
+ *              the user and print it back with
+ *              sentence. 
+ * 
+ * Author: Ahmed Harbi
+ * Date: December 2021
+**************************************************/
+
 #include <iostream>
 
 int main()
 {
-   int favorite_number;
-   std::cout << "Enter your favorite number between 1 and 100: ";
-   std::cin >> favorite_number;
-   std::cout << "Amazing!! That's my favorite number too!" << std::endl;
-   std::cout << "No really!!, " << favorite_number << " is my favorite number!" << std::endl;
+    int favorite_number; // this is where my favorite number is stored
+    std::cout << "Enter your favorite number between 1 and 100: ";
+    std::cin >> favorite_number;
+    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+    std::cout << "No really!!, " << favorite_number << " is my favorite number!" << std::endl;
 
-   return 0;
+    return 0;
 }
