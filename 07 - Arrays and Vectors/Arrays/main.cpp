40793c7b85c4688d20ac6aa0ed44934240e7a581
Ahmed Harbi <ahmed.harbi.eg@gmail.com>
2021-12-04 12:03:22 +0200

    Vectors

--- 07 - Arrays and Vectors/Arrays/main.cpp
+++ 07 - Arrays and Vectors/Arrays/main.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 int main()
 {
-    char vowels[] = {'a', 'e', 'i', 'o', 'u'};
+    char vowels[] {'a', 'e', 'i', 'o', 'u'};
     cout << "\nThe first vowel is: " << vowels[0] << endl;
     cout << "The last vowel is: " << vowels[4] << endl;
 
     // cin >> vowels[5]; out of bounds - do not do this!
 
-    double hi_temps[] = {90.1, 89.8, 77.5, 81.6};
+    double hi_temps[] {90.1, 89.8, 77.5, 81.6};
     cout << "\nThe first high temperature is: " << hi_temps[0] << endl;
 
     hi_temps[0] = 100.7; // set the first element in hi_temp to 100.7
