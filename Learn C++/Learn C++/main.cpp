83473fab282bffa448484bf27a902844a87785cb
Ahmed Harbi <ahmed.harbi.eg@gmail.com>
2021-12-06 12:55:57 +0200

    Challenge

---
+++ Learn C++/Learn C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Learn C++
+//
+//  Created by Ahmed Harbi on 06/12/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
