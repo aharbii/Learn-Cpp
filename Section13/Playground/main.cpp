abbfd5992dfad50fac032b9476a71867dc24c335
Ahmed Harbi <ahmed.harbi.eg@gmail.com>
2021-12-13 18:19:35 +0200

    Update

--- Section13/Playground/main.cpp
+++ Section13/Playground/main.cpp
@@ -5,36 +5,56 @@
 using namespace std;
 
 class Player {
-    // Attributes
-    string name{"Player"};
-    int health{100};
-    int xp{0};
+    public:
+        // Attributes
+        string name{"Player"};
+        int health{100};
+        int xp{0};
 
-    // Methods
-    void talk(string);
-    bool is_dead();
+        // Methods
+        void talk(string text_to_say) { cout << name << " says " << text_to_say << endl; };
+        bool is_dead();
 };
 
 class Account {
-    // attributes
-    string name{"Account"};
-    double balance{0.0};
+    public:
+        // attributes
+        string name{"Account"};
+        double balance{0.0};
 
-    // methods
-    bool deposit(double);
-    bool withdraw(double);
+        // methods
+        void deposit(double bal) { 
+            balance += bal;
+            cout << "In deposit" << endl;
+        };
+
+        void withdraw(double bal) {
+            balance -= bal;
+            cout << "In withdraw" << endl;
+        };
 };
 
 int main()
 {
     Account frank_account;
-    Account jim_account;
-    
+    frank_account.name = "Frank Account";
+    frank_account.balance = 5000.0;
+
+    frank_account.deposit(1000);
+    frank_account.withdraw(500);
+
     Player frank;
-    Player hero;
-    Player player[]{frank, hero};
-    vector<Player> player_vec{frank};
-    player_vec.push_back(hero);
+    frank.name = "Frank";
+    frank.health = 100;
+    frank.xp = 12;
+    frank.talk("Hi there!");
+
+    Player *enemy;
+    enemy = new Player{};
+    (*enemy).name = "Enemy";
+    (*enemy).health = 50;
+    enemy->xp = 15;
+    enemy->talk("I will destroy you!");
     cout << endl;
     return 0;
 }
